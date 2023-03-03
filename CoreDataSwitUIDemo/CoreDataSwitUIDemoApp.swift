d0bc9456b20a49b8a3e143b35fbbd638eb563228
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-03-04 01:09:03 +0600

    Initial Commit

---
+++ CoreDataSwitUIDemo/CoreDataSwitUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataSwitUIDemoApp.swift
+//  CoreDataSwitUIDemo
+//
+//  Created by Joynal Abedin on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataSwitUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
